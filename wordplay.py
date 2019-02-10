659d942fa1e8d376958958e1ee89f2c659a5b684
Matt Phillips <mattman@mattcave.com>
2019-02-10 15:21:47 -0800

    Added standard scrabble scoring of word

--- wordplay.py
+++ wordplay.py
@@ -1,9 +1,24 @@
 #!/usr/bin/env python3
 
+SCRABBLE_VALUES = {
+    0:  " -",
+    1:  "aeilnorstu",
+    2:  "dg",
+    3:  "bcmp",
+    4:  "fhvwy",
+    5:  "k",
+    8:  "jx",
+    10: "qz",
+}
+
+letter_value = {}
+for (value, chars) in SCRABBLE_VALUES.items():
+    for c in chars:
+        letter_value[c] = value
+
 
 def score_word(word):
     score = 0
-    for letter in word:
-        # add code here
-        pass
+    for char in word:
+        score += letter_value.get(char, 0)
     return score
