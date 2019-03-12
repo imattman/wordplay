c5a320c7d30b6b5cd7fd795b90c508950785ffbd
Matt Phillips <mattman@mattcave.com>
2019-03-12 14:44:39 -0700

    Added /api/v1/words/{word} end point
    - added end point that uses Lexicon object for lookup
    - updated main index to use jinja template instead of hardcoded string

--- app.py
+++ app.py
@@ -1,35 +1,49 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, render_template
+import os
+import pathlib
 import wordplay
 
-
+dir_path = os.path.dirname(os.path.realpath(__file__))
+word_file = pathlib.Path(dir_path) / 'sowpods.txt'
+word_list = wordplay.load_lexicon_file(word_file)
+lexicon = wordplay.Lexicon(word_list)
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return """<h1>Flask is running!</h1>
-    <p>Try accessing the <a href="/api/v1/data">dummy data</a> endpoint.</p>
-    <p>Score letters <a href="/api/v1/score?l=wombat">'wombat'</a></p>
-    """
+    return render_template('index.html',
+                           letters=['qwerty', 'asdf'],
+                           words=['apple', 'wombat', 'xylophone'],
+                           missing=['cray cray', 'woozle', 'nerfherder'])
 
 
-@app.route("/api/v1/data")
-def dummy_data():
-    data = {'message': 'looks like it works!',
-            'values': list(range(9))}
-    return jsonify(data)
-
-
-@app.route("/api/v1/score")
-def score_word():
-    letters = request.args.get('l')
-    letters = letters.lower()
+@app.route("/api/v1/score/<letters>")
+def score_word(letters):
+    letters = letters.strip().lower()
     score = wordplay.score_word(letters)
     result = {'letters': sorted(list(letters)),
               'score': score}
     return jsonify(result)
+
+
+@app.route("/api/v1/words/<word>")
+def lookup_word(word):
+    word = word.strip().lower()
+    if word not in lexicon:
+        # return better-than-nothing message along with HTTP 404 NOT FOUND
+        return jsonify({'message': f"'{word}' not found in lexicon"}), 404
+
+    score, word = lexicon[word]
+    return jsonify({'word': word, 'score': score})
+
+
+@app.route("/api/v1/matches/<letters>")
+def find_matches(letters):
+    letters = letters.strip().lower()
+    return jsonify({'message': 'not yet implemented'})
 
 
 def main():
