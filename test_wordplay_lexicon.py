2de9cc800282825a88f5221f36e33fbc4608decd
Matt Phillips <mattman@mattcave.com>
2019-03-12 13:31:41 -0700

    swap actual==expected in tests

--- test_wordplay_lexicon.py
+++ test_wordplay_lexicon.py
@@ -22,19 +22,19 @@ EXPECTED_WORDS = [line.strip().lower() for line in LEXICON_CONTENTS.split()]
 def test_read_words_empty_file():
     file_contents = io.StringIO('')
     words = wordplay.read_words(file_contents)
-    assert [] == words
+    assert words == []
 
 
 def test_read_words_normal():
     file_contents = io.StringIO(LEXICON_CONTENTS)
     words = wordplay.read_words(file_contents)
-    assert EXPECTED_WORDS == words
+    assert words == EXPECTED_WORDS
 
 
 def test_read_words_skip_empty_lines_and_comments():
     contents = LEXICON_CONTENTS.replace(
         '\n', '\n\n# some comment about a word\n')
-    print("lexicon contents:", contents, sep='\n')  # `pytest -s` to see output
+    # print("lexicon contents:", contents, sep='\n')  # `pytest -s` to see output
     file_contents = io.StringIO(contents)
     words = wordplay.read_words(file_contents)
-    assert EXPECTED_WORDS == words
+    assert words == EXPECTED_WORDS
